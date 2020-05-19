90c91fb886d8fd18d345d7a1213dc3e7136b138b
Carina Meissl <carina.meissl@hotmail.com>
2020-05-19 16:37:25 +0200

    rails task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,29 @@
 class TasksController < ApplicationController
 
-  def list
+  def index
+    @tasks = Task.all
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
 
+  def new
+    @task = Task.new
+  end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
 end
